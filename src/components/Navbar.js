6681006730cdd6d517ef93afb299299f3223a426
Dilpreet0501 <100590445+Dilpreet0501@users.noreply.github.com>
2024-07-06 20:19:01 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,115 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ethers } from "ethers";
+import metaMaskIcon from '../assets/metamask-icon.png';  
+// import logo from '../assets/logo.jpg'
 
-const NavbarContainer = styled.div`
+const Nav = styled.nav`
   display: flex;
-  justify-content: center;
-  margin-top: 50px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1b1b1b;
+  padding: 1rem 2rem;
+  color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  position: sticky;
+  top: 0;
+  z-index: 1000;
 `;
 
 const NavLink = styled.a`
-    margin: 0 20px;
-    padding: 10px 20px;
-    color: white;
-    text-decoration: none;
-    border: 2px solid #a99797;
-    border-radius: 26px;
-    position: relative;
-    transition: all 0.3s ease;
-    overflow: hidden;
-    font-family: Calibri;
-    width: 14%;
-    height: 62px;
-    font-size: 37px;
-    align-content: center;
+  color: white;
+  text-decoration: none;
+  font-size: 1rem;
+  margin: 0 1rem;
 
   &:hover {
-    background-color: #282c34;
-    color: white;
-    box-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 20px #f8eeff, 0 0 50px #ffffff;
-
-    &::before {
-      width: 300%;
-    }
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 0;
-    height: 300%;
-    background: rgb(0 0 0 / 0%);
-    transition: width 0.3s ease;
-
+    color: #00ffb9;
   }
 `;
 
+const NavSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.span`
+  width: 40px;
+  height: 40px;
+  font-size: 1.5rem;
+`;
+
+const MetaMaskIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  margin-left: 1rem;
+`;
+
 const Navbar = () => {
+  const [data, setData] = useState({
+    address: "",
+    Balance: null,
+  });
+
+  const btnHandler = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]))
+        .catch((err) => {
+          console.error(err);
+          alert("Error connecting to MetaMask");
+        });
+    } else {
+      alert("Please install MetaMask extension!");
+    }
+  };
+
+  const getBalance = (address) => {
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance) => {
+        setData((prevData) => ({
+          ...prevData,
+          Balance: ethers.utils.formatEther(balance),
+        }));
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Error fetching balance");
+      });
+  };
+
+  const accountChangeHandler = (account) => {
+    setData((prevData) => ({
+      ...prevData,
+      address: account,
+    }));
+    getBalance(account);
+  };
+
+  console.log(data.address);
+  console.log(data.Balance);
+
   return (
-    <NavbarContainer>
-      <NavLink href="#cars">CARS</NavLink>
-      <NavLink href="#bikes">BIKES</NavLink>
-      <NavLink href="#plans">PLANS</NavLink>
-    </NavbarContainer>
+    <Nav>
+      <Logo><b>FinSure</b></Logo>
+      <NavSection>
+        <NavLink href="#">Products</NavLink>
+        <NavLink href="#">Renewals</NavLink>
+        <NavLink href="#">Claims</NavLink>
+        <NavLink href="#">Resources</NavLink>
+        <NavLink href="#">Contact</NavLink>
+        <NavLink href="#">Wallet</NavLink>
+      </NavSection>
+      <NavSection>
+        <MetaMaskIcon src={metaMaskIcon} alt="MetaMask Icon" onClick={btnHandler} />
+      </NavSection>
+    </Nav>
   );
 };
 
---
+++ src/components/welcome.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCar, faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+
+const WelcomeContainer = styled.div`
+  background-color: #0e0e0e;
+  color: white;
+  padding: 5rem 2rem;
+  text-align: center;
+  background-size: cover;
+  background-position: center;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.25rem;
+  margin-bottom: 3rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
+
+const Button = styled.button`
+  background-color: #00ffb9;
+  color: #1b1b1b;
+  border: none;
+  padding: 1rem 2rem;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 0.25rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &:hover {
+    background-color: #00e6a5;
+  }
+`;
+
+const Welcome=()=> {
+  return (
+    <WelcomeContainer>
+      <Title>Welcome to FinSure!!</Title>
+      <Subtitle>Redefining the standard for digital Insurance</Subtitle>
+      <Description>Get quota for Cars and Bikes</Description>
+      <ButtonContainer>
+        <Button>
+          <FontAwesomeIcon icon={faCar} />
+          Cars
+        </Button>
+        <Button>
+          <FontAwesomeIcon icon={faMotorcycle} />
+          Bikes
+        </Button>
+      </ButtonContainer>
+    </WelcomeContainer>
+  );
+}
+
+export default Welcome;
+
+
+
+
+
+
+
+
